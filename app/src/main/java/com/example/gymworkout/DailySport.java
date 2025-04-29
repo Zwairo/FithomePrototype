d503233e358aa3cbff19cbd9276249c80d7465c6
safak <safakaydemirx@gmail.com>
2025-04-29 14:50:14 +0300

    mainlines

--- app/src/main/java/com/example/gymworkout/DailySport.java
+++ app/src/main/java/com/example/gymworkout/DailySport.java
@@ -1,8 +1,6 @@
 package com.example.gymworkout;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,23 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class DailySport extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_daily_sport);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-    public void dailysport(View view){
-        Intent intent = new Intent(this, DailySport.class);
-        startActivity(intent);
-    }
-
 }
